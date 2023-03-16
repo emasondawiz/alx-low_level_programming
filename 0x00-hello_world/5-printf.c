083d5fba9c6c19af7ed39f23592900a613b2bc16
emasondawiz <emaloson4vgc@gmail.com>
2023-03-16 16:19:43 +0100

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Main Entry
+ *
+ * printf: it prints out
+ *
+ * return: Always Present (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
