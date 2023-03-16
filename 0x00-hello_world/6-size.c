315765326622a009508c4e81cab2f350cfa481c8
emasondawiz <emaloson4vgc@gmail.com>
2023-03-16 17:17:31 +0100

    Size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Printf: Print to the screen
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	printf("The size of char is: %d-byte\n", sizeof(char));
+	printf("The size of char is: %d-byte\n", sizeof(int));
+	printf("The size of char is: %d-byte\n", sizeof(float));
+	printf("The size of char is: %d-byte\n", sizeof(double));
+	return (0);
+
+}
