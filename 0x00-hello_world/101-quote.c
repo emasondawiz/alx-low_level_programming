30b4aa04fd9f043bee43ac01735f3a4352d19953
emasondawiz <emaloson4vgc@gmail.com>
2023-03-16 22:39:55 +0100

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Write: print to the standard error
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 70);
+	return (0);
+}
