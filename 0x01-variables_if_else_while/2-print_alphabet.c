84d06942fc0f65487bb04c59d22f7945d049bec9
emasondawiz <emaloson4vgc@gmail.com>
2023-03-17 22:41:39 +0100

    Print Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ *
+ * puts: it print to screen..
+ */
+
+int main(void)
+{
+	char ch = 'B';
+
+	putchar(tolower(ch));
+
+	return (0);
+}
