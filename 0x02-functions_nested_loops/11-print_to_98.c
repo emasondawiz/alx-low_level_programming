e55196431ae8d3bdd59eba4ed76a307eb804a0fd
emasondawiz <emaloson4vgc@gmail.com>
2023-03-22 01:40:45 +0100

    Print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,33 +1,23 @@
 #include <stdio.h>
-
 /**
- * print_to_98 - print numbers from 98 above or below
- *
- * @n: Input variable
- *
- * Return: Always 0 (Success)
- *
+ * print_to_98 - prints all natural numbers from n to 98,
+ * in order separated by a commma followed by a space.
+ * @n: The number to begin counting at.
  */
 
 void print_to_98(int n)
 {
-	int num;
-
 	if (n >= 98)
 	{
-		for (num = n; num > 98; num--)
-		{
-			printf("%d, ", num);
-		}
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
 	}
+
 	else
 	{
-		for (num = n; n < 98; num++)
-		{
-			printf("%d, ", num);
-		}
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
 	}
-
-	printf("98\n");
-
 }
