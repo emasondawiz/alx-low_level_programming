a90c8cfac8a369bed484857c0de942900c5ed95e
emasondawiz <emaloson4vgc@gmail.com>
2023-03-22 15:51:41 +0100

    Lowercase

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print all lowercase alphabet
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	char n;
+
+	for (int i = 0; i < 10; i++)
+	{
+		for (n = 'a'; n <= 'z'; n++)
+		{
+			putchar(n);
+		}
+		putchar('\n');
+	}
+}
