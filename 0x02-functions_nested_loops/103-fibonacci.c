5414c078d7469862f26671a3b437fa530e318fad
emasondawiz <emaloson4vgc@gmail.com>
2023-03-23 00:43:48 +0100

    Fibonacci Part 2

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -14,40 +14,42 @@
 int main(void)
 {
 	int n1, num;
-
-	unsigned long int add, prev, n2;
-
+	unsigned int add, prev, n2, data, sumofeven, limit;
 
 	n1 = 1;
+	data = 40;
+	sumofeven = 0;
 	n2 = 2;
 	add = n1 + n2;
 	prev = n2;
 	num = 0;
-	while (num < 50)
+	limit = 4000000;
+	while (num < data)
 	{
 		if (num == 0)
 		{
-			printf("%d, ", n1);
+			if (n1 % 2 == 0)
+				sumofeven += n1;
+
 		}
 		else if (num == 1)
 		{
-			printf("%ld, ", n2);
+			if (n2 % 2 == 0)
+				sumofeven += n2;
 		}
 		else
 		{
-			if (num == 49)
-			{
-				printf("%ld\n", add);
-			}
+			if (add <= limit)
+				if (add % 2 == 0)
+					sumofeven += add;
 			else
-			{
-				printf("%ld, ", add);
-			}
+				break;
 			prev = n2;
 			n2 = add;
 			add = prev + n2;
 		}
 		num++;
 	}
+	printf("%d\n", sumofeven);
 	return (0);
 }
