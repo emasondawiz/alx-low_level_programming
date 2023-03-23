f3a0a102081015a3a40e917ddd06db1f40759f64
emasondawiz <emaloson4vgc@gmail.com>
2023-03-23 16:05:10 +0100

    Diagonal lines

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,21 +9,23 @@
 
 void print_diagonal(int n)
 {
-	int len, space;
+	int num;
+	int b;
 
 	if (n > 0)
 	{
-		for (len = 0; len < n; len++)
+		for (num = 0; num < n; num++)
 		{
-			for (space = 0; space < len; space++)
-				_putchar('_');
+			for (b = 0; b < num; b++)
+			{
+				_putchar(' ');
+			}
 			_putchar('\\');
-
-			if (len == n - 1)
-				continue;
 			_putchar('\n');
 		}
 	}
-
-	_putchar('\n');
+	else
+	{
+		_putchar('\n');
+	}
 }
