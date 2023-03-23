e46f9918cf4efe4cd9ab71d60fb9b9741411451e
emasondawiz <emaloson4vgc@gmail.com>
2023-03-23 16:25:43 +0100

    Sqaure

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_square - function that displays in square
+ *
+ * @size: the input of print_square
+ *
+ */
+
+void print_square(int size)
+{
+	int a, b;
+
+	if (size > 0)
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size; b++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
