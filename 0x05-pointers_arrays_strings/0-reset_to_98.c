21365e385dc83bab5617ae389343efeb60aaa6ae
emasondawiz <emaloson4vgc@gmail.com>
2023-03-27 15:05:58 +0100

    Reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Function that reset
+ *
+ * @n: Input value
+ *
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+#!/bin/bash
+This directory is all about pointers, string and array
