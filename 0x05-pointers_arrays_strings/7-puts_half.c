c195e2e770eda7d86c62067f9bc0c9a7cb5730d9
emasondawiz <emaloson4vgc@gmail.com>
2023-03-27 20:53:52 +0100

    Put half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * puts_half - function that returns half of the string
+ *
+ * @str: input value
+ *
+ */
+
+void puts_half(char *str)
+{
+	int len = strlen(str);
+
+	int new;
+
+	if (len % 2 != 0)
+	{
+		new = (len - 1) / 2;
+	}
+	else
+	{
+		new = len / 2;
+	}
+	for (int i = new; i <= len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
