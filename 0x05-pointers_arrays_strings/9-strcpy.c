1759b42e3f0008847e8ff5843e93f13863358757
emasondawiz <emaloson4vgc@gmail.com>
2023-03-27 21:51:58 +0100

    Copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strcpy - function that copies
+ *
+ * @dest: Input value
+ *
+ * @src: Input value
+ *
+ * Return: Always copy
+ *
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest, src));
+}
