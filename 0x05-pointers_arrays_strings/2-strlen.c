4d6c43026f70770c401be2abcda8162b7b023a7c
emasondawiz <emaloson4vgc@gmail.com>
2023-03-27 15:53:32 +0100

    Strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen - Count the number of string
+ *
+ * @s: input variable
+ *
+ * Return: it returns an integer
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
