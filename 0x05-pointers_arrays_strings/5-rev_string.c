8dafdfb8e2039a97683b1e429b9849287e0d0bcb
emasondawiz <emaloson4vgc@gmail.com>
2023-03-28 14:50:44 +0100

    Reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - it prints the reverse of a string
+ *
+ * @s: input variable
+ *
+ */
+
+void rev_string(char *s)
+{
+	int len = strlen(s) - 1;
+
+	int j, i;
+
+	char content;
+
+	for (j = len, i = 0; j >= 0; j--, i++)
+	{
+		content = s[j];
+		s[j] = s[i];
+		s[i] = content;
+	}
+
+}
