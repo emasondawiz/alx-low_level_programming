fe994e4a310a2e52802d614064b0abd3233f0451
emasondawiz <emaloson4vgc@gmail.com>
2023-03-29 16:44:12 +0100

    String to Integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,44 @@
+
+#include "main.h"
+
+/**
+ * _atoi - function that convert string to integer
+ *
+ * @s : Input variable
+ *
+ * Return: Always 0
+ *
+ */
+
+int _atoi(char *s)
+{
+	int len, minuscheck, num, i;
+
+	len = strlen(s);
+	minuscheck = 0;
+	num = 0;
+
+	for (i = 0; i < len ; i++)
+	{
+		if (s[i] == '-')
+			minuscheck++;
+
+		if (isdigit(s[i + 1]))
+			break;
+	}
+
+	for (i = 0; i < len ; i++)
+	{
+		if (isdigit(s[i]))
+		{
+			num = (num * 10) + (s[i] - '0');
+			if (!isdigit(s[i + 1]))
+				break;
+		}
+	}
+
+	if (minuscheck % 2 == 1)
+		num = -num;
+
+	return (num);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -13,3 +14,4 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 void rev_string(char *s);
+int _atoi(char *s);
