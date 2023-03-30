b1c5a6005016eb1346d31524fa08f94e8437a669
emasondawiz <emaloson4vgc@gmail.com>
2023-03-30 12:24:31 +0100

    Comparing two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strcmp - Combines two string
+ *
+ * @s1: Input variable
+ * @s2: Input variable
+ *
+ * Return: Always 0
+ *
+ */
+
+int *_strcmp(char *s1, char *s2)
+{
+	return (strcat(s1, s2));
+}
