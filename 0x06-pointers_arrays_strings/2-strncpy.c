40a6618d4607153c9797aa3d93337d5a0b3adbd8
emasondawiz <emaloson4vgc@gmail.com>
2023-03-30 12:20:49 +0100

    Copy some parts of a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strncpy - Combines two string
+ *
+ * @dest: Input variable
+ * @src: Input variable
+ * @n: Input variable
+ *
+ * Return: Always 0
+ *
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	return (strncpy(dest, src, n));
+}
---
+++ 0x06-pointers_arrays_strings/2-strncpy.c~
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strncat - Combines two string
+ *
+ * @dest: Input variable
+ * @src: Input variable
+ * @n: Input variable
+ *
+ * Return: Always 0
+ *
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	return (strncat(dest, src, n));
+}
