fd872e4dabd492ec956dca4fbeb4ecf1b563ca65
emasondawiz <emaloson4vgc@gmail.com>
2023-03-30 13:33:24 +0100

    Reverse

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -14,10 +14,14 @@ void reverse_array(int *a, int n)
 
 	n = n - 1;
 
+	i = 0
+
 	while (i <= n)
 	{
 		arr = a[i];
-		a[i++] = a[n];
-		a[n--] = arr;
+		a[i] = a[n];
+		a[n] = arr;
+		i++;
+		n--;
 	}
 }
