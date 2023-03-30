54c07ea94d44ae52cc809c63b0fcc2657b8eec13
emasondawiz <emaloson4vgc@gmail.com>
2023-03-30 12:10:31 +0100

    Combine two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strcat - Combines two string
+ *
+ * @dest: Input variable
+ * @src: Input variable
+ *
+ * Return: Always 0
+ *
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
---
+++ 0x06-pointers_arrays_strings/main.h~
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+char *_strcat(char *dest, char *src);
