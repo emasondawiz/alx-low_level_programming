3707427f963c7bbc6018ccc008e21cc16c05b0a9
emasondawiz <emaloson4vgc@gmail.com>
2023-04-03 23:29:34 +0100

    Strche

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * *_strchr - function that prints the first occurence of a word
+ * @s: Input value
+ * @c: Input value
+ * Return: Always 0
+ */
+
+char *_strchr(char *s, char c)
+{
+	return (strchr(s, c));
+}
