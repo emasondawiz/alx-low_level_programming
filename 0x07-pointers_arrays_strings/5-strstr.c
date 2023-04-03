af1b9826934450429c94b3aa3c99a97d982fbb37
emasondawiz <emaloson4vgc@gmail.com>
2023-04-03 23:54:45 +0100

    StrStr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * *_strstr - function that get the first occurence and prints everything
+ * @haystack: Input variable
+ * @needle: Input variable
+ * Return: Always 0
+ *
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	return (strstr(haystack, needle));
+}
