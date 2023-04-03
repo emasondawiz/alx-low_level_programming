1814c0629a1852c04082eed2171d24a7505dff8b
emasondawiz <emaloson4vgc@gmail.com>
2023-04-03 23:47:03 +0100

    Strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * *_strpbrk - Function that searches for a pattern
+ * @s: Input value
+ * @accept: Input value
+ * Return: Always 0
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	return (strpbrk(s, accept));
+}
