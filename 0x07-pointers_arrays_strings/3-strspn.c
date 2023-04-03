8e28ac1f590ebb5e6438ebfc2e83d4c6d681af5d
emasondawiz <emaloson4vgc@gmail.com>
2023-04-03 23:40:25 +0100

    Strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strspn - Function that gets positions
+ * @s: Input value
+ * @accept: Input value
+ * Return: Always 0
+ */
+
+
+unsigned int _strspn(char *s, char *accept)
+{
+	return (strspn(s, accept));
+
+}
