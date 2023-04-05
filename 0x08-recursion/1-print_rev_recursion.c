22e5e43bcc95ccddfad3cc8fc113a2df187e5fb6
emasondawiz <emaloson4vgc@gmail.com>
2023-04-05 17:03:44 +0100

    Reverse using recursive

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - It print the reverse of a string
+ *
+ * @s: Input value
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\0');
+	}
+	else
+	{
+		s++;
+		_print_rev_recursion(s);
+		_putchar(*s);
+	}
