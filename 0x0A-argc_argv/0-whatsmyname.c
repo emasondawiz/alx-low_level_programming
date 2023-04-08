da02f8bb9101fe599d28e5eb9f476b70487ac8ac
emasondawiz <emaloson4vgc@gmail.com>
2023-04-08 10:29:09 +0100

    Whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: Input value
+ * @argv: Input
+ * Return: Always success
+ */
+int main(int argc, char *argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+#!/bin/bash
+This is a directory for argc(argument count in an array) and argv (is the array that contains all the argument)
