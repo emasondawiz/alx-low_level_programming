3e47a8c18d3063778d1d7fbdedd2f34505d667d1
emasondawiz <emaloson4vgc@gmail.com>
2023-04-08 12:05:55 +0100

    Argument count while making use of argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,12 +8,9 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
-
-	for (i = 0; i < argc; i++)
+	if (argv)
 	{
-		printf("%s\n", argv[i]);
+		printf("%d\n", argc - 1);
 	}
-	printf("%d\n", argc - 1);
 	return (0);
 }
