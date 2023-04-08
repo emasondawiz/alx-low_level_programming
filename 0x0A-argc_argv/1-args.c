7a7ece537d16af641c479f2262eb6164d4e872ac
emasondawiz <emaloson4vgc@gmail.com>
2023-04-08 10:38:15 +0100

    Argument count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: Input value
+ * @argv: Input
+ * Return: Always success
+ */
+int main(int argc, char *argv)
+{
+	printf("%s\n", argc);
+	return (0);
+}
