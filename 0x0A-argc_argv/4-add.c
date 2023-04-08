ebbf787685690c47d527c6ea011e1b8a0726e14a
emasondawiz <emaloson4vgc@gmail.com>
2023-04-08 20:03:09 +0100

    Add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: Argument Counts
+ * @argv: Arrays of argument
+ * Return: Always success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, sum;
+
+	sum = 0;
+
+	if (argc - 1 == 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		if (atoi(argv[i]) == 0)
+		{
+			puts("Error");
+			return (1);
+		}
+		else
+		{
+			sum = sum + atoi(argv[i]);
+		}
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
