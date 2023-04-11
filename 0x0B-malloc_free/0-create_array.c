d4a34ffa558c8637d56968e15332721cb5b4734b
emasondawiz <emaloson4vgc@gmail.com>
2023-04-11 14:44:21 +0100

    Malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *create_array - Create an array with dynamic memory
+ * @size: Input value
+ * @c: Input value
+ * Return: Always success
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+
+	char *mychar = (char *)malloc(size * sizeof(char));
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		mychar[i] = c;
+	}
+	return (mychar);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,2 @@
+#!/bin/bash
+This is the directory that contains project based on malloc, calloc and realloc
