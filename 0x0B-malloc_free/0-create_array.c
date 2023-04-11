027febdc9df0cf41fbfec52e13a435f8d3cb6016
emasondawiz <emaloson4vgc@gmail.com>
2023-04-11 14:51:37 +0100

    Malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,7 @@ char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
 
-	char *mychar = (char *)malloc(size * sizeof(char));
+	char *mychar = (void *)malloc(size * sizeof(char));
 
 	if (size == 0)
 	{
