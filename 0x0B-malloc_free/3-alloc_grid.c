2b0b114a6402ae066f0c695ad066133bf0ad1492
emasondawiz <emaloson4vgc@gmail.com>
2023-04-12 03:29:02 +0100

    My Matrix

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,30 +9,36 @@
 
 int **alloc_grid(int width, int height)
 {
-	int **grid;
+	int **mymat;
 	int i, j;
 
 	if (width <= 0 || height <= 0)
-	return (NULL);
-
-	grid = malloc(height * sizeof(int *));
-
-	if (grid == NULL)
+	{
 		return (NULL);
+	}
 
+	mymat = (int **) malloc(height * sizeof(int *));
+
+	if (mymat == NULL)
+	{
+		return (NULL);
+	}
 	for (i = 0; i < height; i++)
 	{
-		grid[i] = malloc(width * sizeof(int));
-		if (grid[i] == NULL)
+		mymat[i] = (int *) malloc(width * sizeof(int));
+		if (mymat[i] == NULL)
 		{
 			for (i = 0; i < height; i++)
-				free(grid[i]);
-			free(grid);
+			{
+				free(mymat[i]);
+			}
+			free(mymat);
 			return (NULL);
 		}
 		for (j = 0; j < width; j++)
-			grid[i][j] = 0;
+		{
+			mymat[i][j] = 0;
+		}
 	}
-
-	return (grid);
+	return (mymat);
 }
