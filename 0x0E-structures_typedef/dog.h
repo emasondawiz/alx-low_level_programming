2478c4ff258715235441d8c8172b7f646d2fe754
emasondawiz <emaloson4vgc@gmail.com>
2023-04-17 23:15:27 +0100

    Dog Struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef EMASON
+#define EMASON
+#include <stdio.h>
+
+
+
+/**
+ * struct dog - Dog struct
+ * @name: Input value
+ * @age: Input value
+ * @owner: Input
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
