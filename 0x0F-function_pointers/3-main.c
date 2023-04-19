51bc7649e7e0c240fbff3649658adfe039f40198
emasondawiz <emaloson4vgc@gmail.com>
2023-04-20 00:16:34 +0100

    Calculator

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,42 @@
+#include "3-calc.h"
+
+
+/**
+ * main - Entry point
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return: Always success
+ */
+
+int main(int argc, char *argv[])
+{
+	int calc, num1, num2;
+	char *operator;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	operator = argv[2];
+
+	if (get_op_func(operator) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((*operator == '/' && num2 == 0) || (*operator == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	calc =get_op_func(operator)(num1, num2);
+
+	printf("%d\n", calc);
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,60 @@
+#include "3-calc.h"
+/**
+ * op_add - addition function
+ * @a: Input value
+ * @b: Input value
+ * Return: Always success
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - addition function
+ * @a: Input value
+ * @b: Input value
+ * Return: Always success
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - addition function
+ * @a: Input value
+ * @b: Input value
+ * Return: Always success
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - addition function
+ * @a: Input value
+ * @b: Input value
+ * Return: Always success
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - addition function
+ * @a: Input value
+ * @b: Input value
+ * Return: Always success
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
