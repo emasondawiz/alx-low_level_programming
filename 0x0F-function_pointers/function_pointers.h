bdac7d118783c760d784df2d611158261f9eae56
emasondawiz <emaloson4vgc@gmail.com>
2023-04-19 20:16:30 +0100

    Array Iteration

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,9 @@
 #ifndef EMASON
 #define EMASON
 #include <stdlib.h>
+#include <stdio.h>
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
