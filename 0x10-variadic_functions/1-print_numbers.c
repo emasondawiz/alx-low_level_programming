3ac72a8e787c6edb07f4c095b22d389a71c7ff1f
emasondawiz <emaloson4vgc@gmail.com>
2023-04-20 20:20:30 +0100

    Print Number

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - Print Numbers
+ * @n: Input value
+ * @separator: Seperator
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list myaps;
+
+	if (separator == NULL || n == 0)
+	{
+		return;
+	}
+
+	va_start(myaps, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (i + 1 == n)
+		{
+			printf(" %d\n", va_arg(myaps, int));
+		}
+		else
+		{
+			printf("%d%s", va_arg(myaps, int), separator);
+		}
+	}
+	va_end(myaps);
+}
