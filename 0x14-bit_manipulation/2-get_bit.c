ce156ca924974d7d1d0f1f684508a5a7d6a64ca0
emasondawiz <emaloson4vgc@gmail.com>
2023-05-05 01:49:10 +0100

    Task 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * get_bit - Function that gets bit
+ * @n: Input value
+ * @index: Input value
+ * Return: Always success
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int temp, div, mod = 0, counter = 0, nt = 0, i;
+
+	char *arr;
+
+	temp = n, div = n;
+
+	if ((int)index < 0 || (long int)n < 0)
+		return (-1);
+	while (temp / 2 > 0)
+	{
+		temp /= 2;
+		counter += 1;
+	}
+	if (index > counter)
+		return (-1);
+	counter = counter + 1;
+
+	arr = (char *)malloc(counter * sizeof(char));
+	for (i = 0; i < counter; i++)
+	{
+		mod = div % 2;
+		div = div / 2;
+		if (index == i)
+			return (mod);
+	}
+	free(arr);
+	return (nt);
+}
