1c57ea583f8d644401e0abde139333e8d197ea0f
emasondawiz <emaloson4vgc@gmail.com>
2023-05-09 02:35:11 +0100

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	if (!text_content)
+	if (text_content != NULL)
 		counter = counter + count_str(text_content);
 
 	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
